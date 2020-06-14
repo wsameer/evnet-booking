6824a3ed5a49bb448046ae6b665d2ff5ef379992
nevrmore <dev.wsameer@gmail.com>
2020-06-14 09:36:37 +0530

    added app pages

---
+++ src/pages/Booking.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Booking = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+Booking.propTypes = {
+
+}
+
+export default Booking
---
+++ src/pages/Listing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Listing = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Listing.propTypes = {
+
+}
+
+export default Listing
